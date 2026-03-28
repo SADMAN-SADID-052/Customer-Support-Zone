3d92f0a827f8d6d8524374eeda5fb2b1c7f8af79
Sadman-Sadid-CSE8 <sadidsadman160@gmail.com>
2026-03-28 20:54:39 +0600

    Project setup and fake json data create

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     
+
+     <div>
+      <h1 className='text-5xl font-bold text-blue-400'>Hello App</h1>
+
+      
+     </div>
+    </>
+  )
+}
+
+export default App
